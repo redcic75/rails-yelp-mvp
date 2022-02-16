2325e56d3a176fbd17ecd886aef3b2a093e77a8c
Cedric Louyot <cedric.louyot@gmail.com>
2022-02-16 11:57:44 +0100

    Restaurant controller done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian] }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Start seeds.rb'
+
+puts 'Erasing Restaurant table'
+Restaurant.destroy_all
+
+puts 'Creating restaurants'
+
+5.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name,
+                                 address: Faker::Address.street_address,
+                                 phone_number: Faker::PhoneNumber.phone_number,
+                                 category: Restaurant::CATEGORIES.sample)
+  puts "Restaurant #{restaurant.id} created"
+end
+
+puts 'Seeding done'
